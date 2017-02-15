573360d7292a1f817cce5e785fb14427a59a23f2
Will Hogan <willhogan11@hotmail.com>
2017-02-15 17:11:13 +0000

    Completed function that picks 6 random numbers from a list of availables

--- countdown.rkt
+++ countdown.rkt
@@ -13,7 +13,8 @@
 ; random is a reserved racket word and can take a single param, or two which is what i'm using here. 
 (define targetNumber (random 101 999))
 
-; Test display the value from the function defined above
+
+; Display the value from the function defined above, the Target Number
 (display "Target number : ")
 targetNumber
 
@@ -22,26 +23,33 @@ targetNumber
 ;[1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 25, 50, 75, 100]
 (define availableNums(list 1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 25 50 75 100))
 
+(display "Your Available numbers are : ")
+availableNums
+
 
 ; Choose 6 random numbers from the above list. Duplicates are allowed in the list, e.g. [1,4,10,10,25,2]
-; Each number can only be used once....
-; Will need to get the user to input these from the command line?
 
-; (build-list 24 values)
-
-
-(define sixRandNums
-  (list-ref availableNums
-    (random 24)))
-
-
-sixRandNums
-
-
+; First Define a function that takes a list as an argument and returns a random number from that list. 
 (define (random-element list)
- (list-ref list(random (length list))))
+  (list-ref list (random (length list))))
 
-; (define popList (random-element availableNums))
+
+
+; Next we define a function that appends each randomly selected number from the available list
+; of numbers, to a new list. This happens 6 six times, once for each of the numbers.
+; NOTE: This works, however it's inefficient. My plan (time permitting) will be to try and work in a recursive algorithm that will optimise the code
+; to allow each one of the numbers to be added to the list, which will a be more programatically efficent and cut down on the repeating code. 
+(define randomSixNumbers
+(append (list (random-element availableNums)
+              (random-element availableNums)
+              (random-element availableNums)
+              (random-element availableNums)
+              (random-element availableNums)
+              (random-element availableNums))))
+
+
+(display "You have Randomly selected your 6 numbers, they are : ")
+randomSixNumbers
 
 
 
